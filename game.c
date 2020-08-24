dd6ae86d74fe7352176cb0270569b6094d0e878c
Harry <harrykeartland@gmail.com>
2020-08-24 18:37:59 +0100

    added brute force check for win

--- game.c
+++ game.c
@@ -29,7 +29,19 @@ void doXGo(){
 void doOGo(){
 
 }
+//returns wether the game has been won or drawn
 int checkIfContinue(){
+    //rows
+    if (board[0] == board[1] && board[0]==board[2] && board[0] != 32) return 0;
+    if (board[3] == board[4] && board[3]==board[5] && board[3] != 32) return 0;
+    if (board[6] == board[7] && board[6]==board[8] && board[6] != 32) return 0;
+    //columns
+    if (board[0] == board[3] && board[0]==board[6] && board[0] != 32) return 0;
+    if (board[1] == board[4] && board[1]==board[7] && board[1] != 32) return 0;
+    if (board[2] == board[5] && board[2]==board[8] && board[2] != 32) return 0;
+    //diags
+    if (board[0] == board[4] && board[0]==board[8] && board[0] != 32) return 0;
+    if (board[2] == board[4] && board[2]==board[6] && board[2] != 32) return 0;
     return 1;
 }
 int main(){
