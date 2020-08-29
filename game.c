5eddf6d1f39364065a0048d0e825ff6a0effd5ee
Harry <harrykeartland@gmail.com>
2020-08-29 15:25:34 +0100

    refactored players goes

--- game.c
+++ game.c
@@ -12,48 +12,52 @@ void drawBoard(){
         printf("%d: %c | %c | %c \n",1+(i+1)/3, board[i], board[i+1], board[i+2]);
     }
 }
-void doXGo(){
+void doGo(char letter){
     int entered=0;
     char pos[4];
     do{
-        printf("please input where you want to put your x[example a1]: ");
+        printf("please input where you want to put your %c[example a1]: ", letter);
         fgets(pos,4,stdin);
         if(pos[0] >= 'a' && pos[0] <= 'c' && pos[1] > '0' && pos[1] <= '3'){
             entered = 1;
         }
     } while(!entered);
     printf("%d",(pos[0]-'a')+3*(pos[1]-'1'));
-    board[(pos[0]-'a')+3*(pos[1]-'1')] = 'x';
+    board[(pos[0]-'a')+3*(pos[1]-'1')] = letter;
     printf("you entered %s\n", pos);
 }
 void doOGo(){
-
 }
-//returns wether the game has been won or drawn
-int checkIfContinue(){
+int getValue(){
+    
+}
+//returns wether the game is over by saying the player that wins, 0 is continue, 120 is player x, 111 is player o
+int checkIfContinue(char checkBoard[10]){
     //rows
-    if (board[0] == board[1] && board[0]==board[2] && board[0] != 32) return 0;
-    if (board[3] == board[4] && board[3]==board[5] && board[3] != 32) return 0;
-    if (board[6] == board[7] && board[6]==board[8] && board[6] != 32) return 0;
+    if (checkBoard[0] == checkBoard[1] && checkBoard[0]==checkBoard[2] && checkBoard[0] != 32) return checkBoard[0];
+    if (checkBoard[3] == checkBoard[4] && checkBoard[3]==checkBoard[5] && checkBoard[3] != 32) return checkBoard[3];
+    if (checkBoard[6] == checkBoard[7] && checkBoard[6]==checkBoard[8] && checkBoard[6] != 32) return checkBoard[6];
     //columns
-    if (board[0] == board[3] && board[0]==board[6] && board[0] != 32) return 0;
-    if (board[1] == board[4] && board[1]==board[7] && board[1] != 32) return 0;
-    if (board[2] == board[5] && board[2]==board[8] && board[2] != 32) return 0;
+    if (checkBoard[0] == checkBoard[3] && checkBoard[0]==checkBoard[6] && checkBoard[0] != 32) return checkBoard[0];
+    if (checkBoard[1] == checkBoard[4] && checkBoard[1]==checkBoard[7] && checkBoard[1] != 32) return checkBoard[1];
+    if (checkBoard[2] == checkBoard[5] && checkBoard[2]==checkBoard[8] && checkBoard[2] != 32) return checkBoard[2];
     //diags
-    if (board[0] == board[4] && board[0]==board[8] && board[0] != 32) return 0;
-    if (board[2] == board[4] && board[2]==board[6] && board[2] != 32) return 0;
-    return 1;
+    if (checkBoard[0] == checkBoard[4] && checkBoard[0]==checkBoard[8] && checkBoard[0] != 32) return checkBoard[0];
+    if (checkBoard[2] == checkBoard[4] && checkBoard[2]==checkBoard[6] && checkBoard[2] != 32) return checkBoard[2];
+    return 0;
 }
 int main(){
     for(int i=0;i<10;i++){
         board[i] = ' ';
     }
-    running=1;
-    while(running){
+    int ended=0;
+    char cur = 'x';
+    while(!ended){
         drawBoard();
-        doXGo();
-        doOGo();
-        running = checkIfContinue();
-        printf("score: %d\n", running);
+        doGo(cur);
+        ended = checkIfContinue(board);
+        if (cur == 'x') { cur = 'o' }
+        else { cur = 'x' }
     }
+    printf("\nwinner: %c\n", ended);
 }
